08f9f2b2d25c8d8d29712ec35fd8cc2708be8969
nickzmoro <nicolasmororuiz@gmail.com>
2025-06-25 20:43:14 -0300

    feat: Introduce new Header component and update assets; replace HeaderIntro with enhanced functionality and add new images for display.

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { TbWorld } from "react-icons/tb";
+
+const HeaderIntro = ({ hasLogged }) => {
+  return (
+    <header className="fixed top-5 left-0 flex items-center justify-around w-full">
+      <div className="flex items-center gap-3">
+        <img src="./logo.png" alt="Logo" className="w-[35px] h-[35px]" />
+        <p className="text-[#8057FE] text-[1.5rem] font-extrabold">
+          &lt;dev
+          <span className="text-[#6753A6]">lingo/&gt;</span>
+        </p>
+      </div>
+      {hasLogged ? (
+        <div className="flex items-center gap-4">
+          <div className="flex items-center gap-1.5">
+            <img src="./ofensive-day.png" alt="Foguinho" className="w-[18px]" />
+            <span className="font-bold text-[#FF9600] text-[1.2rem]">3</span>
+          </div>
+          <div className="flex items-center gap-1.5">
+            <img src="./cristal.png" alt="Foguinho" className="w-[21px]" />
+            <span className="font-bold text-[#1CB0F6] text-[1.2rem]">150</span>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <TbWorld size={25} className="text-[#c4c4c4] cursor-pointer" />
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default HeaderIntro;
